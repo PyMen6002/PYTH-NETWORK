2a130cae5ce9a7c774900dfe69d2557d9837907a
PyMen <92265050+PyMen6002@users.noreply.github.com>
2025-12-25 19:49:20 +0100

    Add util helpers

--- backend/util/crypto_hash.py
+++ backend/util/crypto_hash.py
@@ -1,14 +1,16 @@
 import hashlib
 import json
 
+
 def crypto_hash(*args):
     """
-    Return a sha-256 hash of the given args.
+    Return a sha-256 hash of the given arguments.
+    Arguments are converted to JSON strings and sorted to ensure deterministic hashing.
     """
-    stringified_args = sorted(map(lambda data: json.dumps(data), args))
-    joined_data = ''.join(stringified_args)
+    stringified_args = sorted(map(lambda data: json.dumps(data, sort_keys=True), args))
+    joined_data = "".join(stringified_args)
+    return hashlib.sha256(joined_data.encode("utf-8")).hexdigest()
 
-    return hashlib.sha256(joined_data.encode('utf-8')).hexdigest()
 
 if __name__ == '__main__':
     print(f"Crypto: {crypto_hash('foo', 'two', 2)}")
